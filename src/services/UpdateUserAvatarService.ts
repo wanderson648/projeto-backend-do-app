888ccf7a7183c3d2304621dc9578022d089a7a7f
Wanderson Oliveira <testepedroca@gmail.com>
2020-11-28 13:25:20 -0300

    criando class de erro

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -1,9 +1,10 @@
 import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
-import User from '../models/User';
-
 import uploadConfig from '../config/upload';
+
+import AppError from '../errors/AppError';
+import User from '../models/User';
 
 /* eslint-disable camelcase */
 interface Request {
@@ -18,7 +19,7 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change avatar.');
+      throw new AppError('Only authenticated users can change avatar.', 401);
     }
 
     if (user.avatar) {
