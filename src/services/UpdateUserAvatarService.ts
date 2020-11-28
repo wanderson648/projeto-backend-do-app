4119d9414e4beb7996f8f2853a9dec5ea1730102
Wanderson Oliveira <testepedroca@gmail.com>
2020-11-28 13:02:23 -0300

    atualizando avatar

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+import User from '../models/User';
+
+import uploadConfig from '../config/upload';
+
+/* eslint-disable camelcase */
+interface Request {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFileName }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExits = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExits) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
